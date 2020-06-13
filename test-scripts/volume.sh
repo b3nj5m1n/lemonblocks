5209b954777e50a0123ec0eac00a5546c5fc75ed
b3nj4m1n <b3nj4m1n@gmx.net>
2020-06-13 23:23:49 +0200

    Fixed volume script

--- test-scripts/volume.sh
+++ test-scripts/volume.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-volume=$(pactl list sinks | grep '^[[:space:]]Volume:' | \
-    head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')
+volume="$(amixer sget Master | grep 'Right:' | awk -F'[][]' '{ print $2 }' | cut -d '%' -f 1)"
 
 printf "%%{A1:amixer set Master toggle; pkill lemonblocks -6:}\
 %%{A4:amixer -q sset Master 1%%+; pkill lemonblocks -6:}\
 %%{A5:amixer -q sset Master 1%%-; pkill lemonblocks -6:}\
-$volume %% \
+$volume%% \
 %%{A1}%%{A4}%%{A5}"
