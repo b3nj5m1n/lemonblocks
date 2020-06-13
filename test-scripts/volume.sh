f95c33a98008418d6b92b7069c0467573d905966
b3nj4m1n <b3nj4m1n@gmx.net>
2020-06-13 21:05:45 +0200

    Added volume script

--- test-scripts/volume.sh
+++ test-scripts/volume.sh
@@ -3,4 +3,8 @@
 volume=$(pactl list sinks | grep '^[[:space:]]Volume:' | \
     head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')
 
-printf "$volume"
+printf "%%{A1:amixer set Master toggle; pkill lemonblocks -6:}\
+%%{A4:amixer -q sset Master 1%%+; pkill lemonblocks -6:}\
+%%{A5:amixer -q sset Master 1%%-; pkill lemonblocks -6:}\
+$volume%%\
+%%{A1}%%{A4}%%{A5}"
